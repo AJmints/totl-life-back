ce628fd73c45ad9eb3d9341900874dc3f6e87000
AJmints <alexjmints@gmail.com>
2023-10-25 17:59:16 -0500

    chang driver class in app prop

--- src/main/java/life/totl/totlback/TotlBackApplication.java
+++ src/main/java/life/totl/totlback/TotlBackApplication.java
@@ -2,10 +2,8 @@ package life.totl.totlback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class TotlBackApplication {
 
 	public static void main(String[] args) {
