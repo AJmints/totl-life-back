7131f510a0b0bd8e64d5c6ddfe436add25d098ca
AJmints <alexjmints@gmail.com>
2023-10-25 17:53:08 -0500

    exclude config stuff in main

--- src/main/java/life/totl/totlback/TotlBackApplication.java
+++ src/main/java/life/totl/totlback/TotlBackApplication.java
@@ -2,10 +2,10 @@ package life.totl.totlback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TotlBackApplication {
 
 	public static void main(String[] args) {
